ea2ca01dc79d73ebf9783600c51de9559e3d435c
Christian Simonsen <crsdev@gmail.com>
2023-09-20 11:39:15 +0200

    org tests

---
+++ tests/testRandomGenerator.mjs
@@ -0,0 +1,38 @@
+import { test } from "./testFramework.mjs";
+import { createRandomNumber } from "../randomUtil.mjs";
+
+test(() => {
+  createRandomNumber();
+  return true;
+}, "Creating random number");
+
+test(() => {
+  return typeof createRandomNumber() === "number";
+}, "number is returned ");
+
+function test_doesFunctionReturnValuesBetweent() {
+  const min = 0;
+  const max = 10;
+
+  for (let i = 0; i < 10000; i++) {
+    const result = createRandomNumber();
+    if (result > max || result < min) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+test(test_doesFunctionReturnValuesBetweent, "Values fall within limits");
+
+test(() => {
+  let results = new Set();
+
+  for (let i = 0; i < 10000; i++) {
+    let num = createRandomNumber();
+    results.add(num);
+  }
+
+  return results.size > 5000;
+}, "Numbers are random");
